4b0d4cbe66edc2214ddab52ddb7f0245563f77fe
paranoodle <preseada@hotmail.com>
2015-05-01 11:34:23 +0200

    ???

--- riviere.h
+++ riviere.h
@@ -1,12 +1,23 @@
+#include <cstdlib>
 #include <string>
+#include <list>
+
+using namespace std;
 
 class Container {
 private:
     string name;
-    list<Person*> people;
 public:
+    list<Person*> people;
+    Container(string name) : name(name) {}
     Container(string name, list<Person*> people) : name(name), people(people) {}
     ~Container();
+};
+
+class Bank : public Container {
+public:
+    Bank(string name) : Container(name) {}
+    ~Bank();
 };
 
 class Boat : public Container {
@@ -15,12 +26,6 @@ private:
 public:
     Boat(string name) : Container(name) {}
     ~Boat();
-};
-
-class Bank : public Container {
-public:
-    Bank(string name) : Container(name) {}
-    ~Bank();
 };
 
 class Person {
@@ -39,7 +44,40 @@ public:
     bool canDrive() const { return true; }
 };
 
+class Mere : public Person {
+public:
+    Mere() : Person("mere") {}
+    ~Mere();
+    bool canDrive() const { return true; }
+};
 
+class Fils : public Person {
+public:
+    Fils(string name) : Person(name) {}
+    ~Fils();
+    bool canDrive() const { return false; }
+};
+
+class Fille : public Person {
+public:
+    Fille(string name) : Person(name) {}
+    ~Fille();
+    bool canDrive() const { return false; }
+};
+
+class Policier : public Person {
+public:
+    Policier() : Person("policier") {}
+    ~Policier();
+    bool canDrive() const { return true; }
+};
+
+class Voleur : public Person {
+public:
+    Voleur() : Person("voleur") {}
+    ~Voleur();
+    bool canDrive() const { return false; }
+};
 
 class Controller {
 private:
